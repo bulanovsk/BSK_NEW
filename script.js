b57dc5e9bdb18961e3c2169e7f8d55ab751a2149
bulanovsks <ctepan.bulanov@yandex.ru>
2026-02-03 21:37:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Обработчики для ссылок меню
 document.getElementById('profileLink').addEventListener('click', function(e) {
     e.preventDefault();
-    toggleMenu(); // Закрываем меню
+    toggleMenu(); 
     setTimeout(() => {
         window.location.href = 'register.html';
     }, 300);
@@ -9,15 +8,15 @@ document.getElementById('profileLink').addEventListener('click', function(e) {
 
 document.getElementById('helpLink').addEventListener('click', function(e) {
     e.preventDefault();
-    toggleMenu(); // Закрываем меню
+    toggleMenu(); 
     setTimeout(() => {
         window.location.href = 'help.html';
     }, 300);
 });
 
-// Функция переключения меню
 function toggleMenu() {
     dropdownMenu.classList.toggle('active');
     overlay.classList.toggle('active');
     document.body.style.overflow = dropdownMenu.classList.contains('active') ? 'hidden' : '';
+
 }
